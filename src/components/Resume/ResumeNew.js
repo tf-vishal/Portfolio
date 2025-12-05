2297ed6310677e7c9478f7fb41b84ad689f0fb85
tf-vishal <vvishalsharma0609@gmail.com>
2025-12-05 23:48:44 +0530

    finalize

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -37,16 +37,7 @@ function ResumeNew() {
         </Row>
       
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
+        <Row style={{ justifyContent: "center", position: "relative" }}>   
         </Row>
       </Container>
     </div>
